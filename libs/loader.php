5cef315abccc2361bd108d56913f202a20ab2e32
Thoronir42 <Thoronir42@Gmail.com>
2015-10-25 00:08:31 +0200

    Loader bye, autoloader hello

--- libs/loader.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace clh;
-
-require __DIR__.'/dibi/dibi.php';
-require __DIR__.'/TemplateContainer.php';
-
-require __DIR__.'/../controlers/Controler.php';
-require __DIR__.'/../controlers/HomeControler.php';
