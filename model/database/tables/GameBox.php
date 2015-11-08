514d8f280cb3fa98a9e6d2a020c2d59356287689
Thoronir42 <Thoronir42@Gmail.com>
2015-11-08 12:43:23 +0100

    Extended recognized database table pool.

---
+++ model/database/tables/GameBox.php
@@ -0,0 +1,19 @@
+<?php
+namespace model\database\tables;
+
+/**
+ * Description of User
+ *
+ * @author Stepan
+ */
+class User {
+	
+	var $user_id;
+	
+	var $orion_login;
+	
+	var $name;
+	
+	var $surname;
+	
+}
---
+++ model/database/tables/GameRating.php
@@ -0,0 +1,18 @@
+<?php
+namespace model\database\tables;
+
+/**
+ * Description of User
+ *
+ * @author Stepan
+ */
+class GameRating{
+	
+	var $game_rating_id;
+	
+	var $game_type_id;
+	
+	var $score;
+	
+	var $review;
+}
