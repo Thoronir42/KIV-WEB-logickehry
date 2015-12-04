aaf1e6fc7a23718460eeed8a1f58a883259fb7fa
Thoronir42 <Thoronir42@Gmail.com>
2015-12-04 21:55:22 +0100

    Created reservation insert page

--- controllers/RezervaceController.php
+++ controllers/RezervaceController.php
@@ -2,10 +2,37 @@
 namespace controllers;
 
 
-class LetisteController extends Controller{
-	
+class RezervaceController extends Controller{
+
 	public function startUp(){
-		
+		parent::startUp();
 	}
+	
+	protected function buildSubmenu() {
+		$menu = [
+			["urlParams" => ["controller" => "rezervace", "action"=>"vypis"],
+				"label" => "Vypis"
+			],
+			["urlParams" => ["controller" => "rezervace", "action"=>"zadat"],
+				"label" => "Zadat"
+			],
+		];
+		return $menu;
+	}
+
+	
+	public function renderVypis(){
+		$this->template["pageTitle"] = "Výpis rezervací";
+		$reservations = $this->pdoWrapper->getReservationsAndAll();
+		
+		$this->template["rezervace"] = $reservations;
+	}
+	
+	public function renderZadat(){
+		$this->template['pageTitle'] = "Zadat novou rezervaci";
+		$this->template['games'] = $this->pdoWrapper->getGameTypes();
+		$this->template['desks'] = $this->pdoWrapper->getDesks();
+	}
+	
 	
 }
