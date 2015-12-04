6c17e1201ec30ceb17a8ab3da84fca785d363785
Thoronir42 <Thoronir42@Gmail.com>
2015-12-04 22:02:01 +0100

    Added submenu to VypisController and enhanced games listing

--- controllers/VypisController.php
+++ controllers/VypisController.php
@@ -7,22 +7,36 @@ namespace controllers;
  * @author Stepan
  */
 class VypisController extends Controller{
-    
+	
+	protected function buildSubmenu(){
+		$menu = [];
+		$menu[] = ["urlParams" => ["controller" => "vypis", "action"=>"hry"],
+				"label" => "Hry"];
+		$menu[] = ["urlParams" => ["controller" => "vypis", "action"=>"krabice"],
+				"label" => "Herní krabice"];
+		$menu[] = ["urlParams" => ["controller" => "vypis", "action"=>"rezervace"],
+				"label" => "Uživatelé"];
+		$menu[] = ["urlParams" => ["controller" => "vypis", "action"=>"inventory"],
+				"label" => "(XML)"];
+		return $menu;
+	}
 	
 	public function startUp(){
 		parent::startUp();
 		$this->layout = 'layout.twig';
-		
 		$this->template['title'] = "CLH";
 		
 	}
 	
     public function renderHry(){
+		$this->addCss("hra.css");
+		$this->template['pageTitle'] = "Výpis her";
         $this->template['hry'] = $this->pdoWrapper->getGamesWithScores();
     }
-    
+	
 	public function renderRezervace(){
 		$this->addCss("vypis_rezervace.css");
+		$this->template['pageTitle'] = "Výpis rezervací";
 		$this->template['rezervace'] = $this->pdoWrapper->getReservationsAndAll();
 		foreach ($this->template['rezervace'][0] as $key => $var){
 			echo "$key => $var<br/>";
