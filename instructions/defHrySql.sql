94b4ccf974677d796c20beee849882d8376a7e0c
Thoronir42 <Thoronir42@Gmail.com>
2015-12-19 17:43:18 +0100

    Repository backup before transer

---
+++ instructions/defHrySql.sql
@@ -0,0 +1,6 @@
+INSERT INTO `web_logickehry_db`.`game_type` (`game_type_id`, `game_name`, `avg_game_time`, `max_players`, `min_players`)
+VALUES 
+	(NULL, 'Abalone Quattro', '30', '2', '4'),
+	(NULL, 'Afrika', '60', '2', '4'),
+	(NULL, 'Agricola', '120', '1', '5'),
+	(NULL, 'Alcatraz (Jumbo)', '30', '1', '1')
