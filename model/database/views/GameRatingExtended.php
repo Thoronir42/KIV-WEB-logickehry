2f215ed17257e15ab2d401a987ff8d091417e4f2
Thoronir42 <Thoronir42@Gmail.com>
2015-12-29 16:24:10 +0100

    Introduced Game Rating related classes that were missing for some reason

---
+++ model/database/views/GameRatingExtended.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Description of GameRatingExtended
+ *
+ * @author Stepan
+ */
+class GameRatingExtended extends \model\database\tables\GameRating {
+
+	var $name;
+	var $orion_login;
+
+}
