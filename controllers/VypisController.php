0461f9c1af4e4c49ee20306163984e8b11ef5bbd
Thoronir42 <Thoronir42@Gmail.com>
2015-12-29 16:01:27 +0100

    Users can now review game types

--- controllers/VypisController.php
+++ controllers/VypisController.php
@@ -41,7 +41,6 @@ class VypisController extends Controller{
 	
 	public function renderDetailHry(){
 		$id = $this->getParam("id");
-		
 		$gameType = GameTypeManager::fetchById($this->pdoWrapper, $id);
 		if(!$gameType){
 			$this->message("Požadovaná hra nebyla nalezena.", \libs\MessageBuffer::LVL_WAR);
@@ -54,10 +53,29 @@ class VypisController extends Controller{
 		// @todo: fetch single subscribed game only
 		$this->user->setSubscribedItems(SubscriptionManager::fetchGamesByUser($this->pdoWrapper, $this->user->user_id));
 		
+		$this->template['form_action'] = ['controller' => 'vypis', 'action' => 'hodnotit', 'id' => $id];
 		$this->template['g'] = $gameType;
 		$this->template['ratings'] = $this->pdoWrapper->gameRatingsByGameType($id);
 		$this->template['rating'] = ['min' => 1, 'def' => 3, 'max' => 5];
 		$this->template['highlight'] = $this->getParam("highlight");
+	}
+	
+	public function doHodnotit(){
+		$is_edit = $this->getParam('edit', INPUT_POST);
+		$pars = ['game_type_id' => $this->getParam('id'),
+			"user_id" => $this->user->user_id,
+			"score" => \model\RatingManager::validate($this->getParam("rating", INPUT_POST)),
+			"review" => $this->getParam("review", INPUT_POST),
+			];
+		
+		if(\model\RatingManager::delete($this->pdoWrapper, $pars['user_id'], $pars['game_type_id'])
+				&& (\model\RatingManager::insert($this->pdoWrapper, $pars))){
+			$this->message($is_edit? "Vaše hodnocení bylo úspěšně upraveno." : "Hodnocení bylo přidáno.", \libs\MessageBuffer::LVL_SUC);
+		} else {
+			$this->message("Při ukládání vašeho hodnocení nastala chyba.", \libs\MessageBuffer::LVL_WAR);
+		}
+		$redirect = ['controller' => 'vypis', 'action' => 'detailHry', 'id' => $pars['game_type_id']];
+		$this->redirect($this->urlGen->url($redirect));
 	}
 	
 }
