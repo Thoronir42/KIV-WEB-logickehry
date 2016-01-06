afd8e5ff3af24627d6b2284306bd4471604c382b
Thoronir42 <Thoronir42@Gmail.com>
2016-01-06 14:06:59 +0100

    Fixed sprava/hry.twig template so that buttons are stretched to fit whole row

--- controllers/SpravaController.php
+++ controllers/SpravaController.php
@@ -1,55 +1,57 @@
 <?php
+
 namespace controllers;
 
 use \model\ImageManager,
 	\model\MailManager;
+use \model\database\tables as Tables,
+	\model\database\views as Views;
 
-use \model\database\tables	as Tables,
-	\model\database\views	as Views;
+class SpravaController extends Controller {
 
-class SpravaController extends Controller{
-	
-	public static function getDefaultAction() { return "hry"; }
-	
-	
+	public static function getDefaultAction() {
+		return "hry";
+	}
+
 	protected function buildSubmenu() {
 		$menu = [];
-		$menu[] = ["urlParams" => ["controller" => "sprava", "action"=>"hry"],
-				"label" => "Hry"];
-		$menu[] = ["urlParams" => ["controller" => "sprava", "action"=>"inventar"],
-				"label" => "Inventář"];
-		if($this->user->isAdministrator()){
+		$menu[] = ["urlParams" => ["controller" => "sprava", "action" => "hry"],
+			"label" => "Hry"];
+		$menu[] = ["urlParams" => ["controller" => "sprava", "action" => "inventar"],
+			"label" => "Inventář"];
+		if ($this->user->isAdministrator()) {
 			$menu[] = ["separator" => true];
-			$menu[] = ["urlParams" => ["controller" => "sprava", "action"=>"uzivatele"],
+			$menu[] = ["urlParams" => ["controller" => "sprava", "action" => "uzivatele"],
 				"label" => "Uživatelé"];
-			$menu[] = ["urlParams" => ["controller" => "sprava", "action"=>"ovladaciPanel"],
-					"label" => "Ovládací panel"];
-			$menu[] = ["urlParams" => ["controller" => "sprava", "action"=>"hromadnyMail"],
-					"label" => "Hromadný mail"];
+			$menu[] = ["urlParams" => ["controller" => "sprava", "action" => "ovladaciPanel"],
+				"label" => "Ovládací panel"];
+			$menu[] = ["urlParams" => ["controller" => "sprava", "action" => "hromadnyMail"],
+				"label" => "Hromadný mail"];
 		}
 		return $menu;
 	}
-	
-	public function startUp(){
+
+	public function startUp() {
 		parent::startUp();
-		if(!$this->user->isSupervisor()){
+		if (!$this->user->isSupervisor()) {
 			$this->message("Do sekce Správa nemáte přístup", \libs\MessageBuffer::LVL_WAR);
 			$this->redirectPars(Controller::DEFAULT_CONTROLLER);
 		}
 	}
-	
-	public function renderHry(){
+
+	public function renderHry() {
 		$this->addCss("hra.css");
 		$this->addCss('input-specific.css');
-		$this->addJs ('input-specific.js');
-		
+		$this->addJs('input-specific.js');
+
 		$this->template['pageTitle'] = "Správa her";
 		$this->template['insert_game_form_action'] = ['controller' => 'sprava', 'action' => 'pridatHru'];
 		$this->template['gpr'] = 3;
-		
-		
+		$this->template['game_edit_form_action'] = ['controller' => 'sprava', 'action' => 'upravitHru'];
+
+
 		$games = Views\GameTypeExtended::fetchAll($this->pdo);
-		foreach($games as $key => $g){
+		foreach ($games as $key => $g) {
 			$path = $this->urlGen->img(ImageManager::get(sprintf("game_%03d", $g->game_type_id)));
 			$games[$key]->picture_path = $path;
 		}
@@ -78,82 +80,81 @@ class SpravaController extends Controller{
 		}
 		$this->redirectPars('sprava', 'hry');
 	}
-	
-	public function renderUzivatele(){
-		if(!$this->user->isAdministrator()){
+
+	public function renderUzivatele() {
+		if (!$this->user->isAdministrator()) {
 			$this->message("Do správy uživatelů nemáte přístup.", \libs\MessageBuffer::LVL_WAR);
 			$this->redirectPars("sprava", $this->getDefaultAction());
 		}
-		
+
 		$this->template['pageTitle'] = "Správa registrovaných uživatelů";
 		$this->template['users'] = Views\UserExtended::fetchAll($this->pdo);
 		$this->template['actions'] = $this->buildUserActions();
 		$this->template['tmpUrl'] = ['controller' => 'sprava', 'action' => 'nan'];
 	}
-	
-	private function buildUserActions(){
+
+	private function buildUserActions() {
 		$act = [];
 		$act['supervisor'] = [
-			'add'	 => ['url' => 'addSupervisor', 'glyph' => 'glyphicon-pencil', 'tooltip' => 'Povýšit na správce', 'class' => 'add'],
+			'add' => ['url' => 'addSupervisor', 'glyph' => 'glyphicon-pencil', 'tooltip' => 'Povýšit na správce', 'class' => 'add'],
 			'remove' => ['url' => 'removeSupervisor', 'glyph' => 'glyphicon-pencil', 'tooltip' => 'Odebrat pravomoce správce', 'class' => 'remove']
 		];
-		
+
 		return ['count' => count($act), 'list' => $act];
 	}
-	
-	public function doAddSupervisor(){
+
+	public function doAddSupervisor() {
 		$this->setUserRole(Tables\User::ROLE_SUPERVISOR);
 	}
-	
-	public function doRemoveSupervisor(){
+
+	public function doRemoveSupervisor() {
 		$this->setUserRole(Tables\User::ROLE_USER);
 	}
-	
-	private function setUserRole($role){
+
+	private function setUserRole($role) {
 		$orion_login = $this->getParam("orion_login");
-		
+
 		$targetUser = Views\UserExtended::fetch($this->pdo, $orion_login);
-		if($targetUser->isAdministrator()){
+		if ($targetUser->isAdministrator()) {
 			$this->message("Administrátoři si vzájemně nemohou upravovat role. Pokud máte problém s $orion_login, kontaktujte prosím CIV.", \libs\MessageBuffer::LVL_DNG);
 			$this->redirectPars('sprava', 'uzivatele');
 		}
-		
-		if(Tables\User::setUserRole($this->pdo, $orion_login, $role)){
-			if($role == Tables\User::ROLE_SUPERVISOR){
+
+		if (Tables\User::setUserRole($this->pdo, $orion_login, $role)) {
+			if ($role == Tables\User::ROLE_SUPERVISOR) {
 				$this->message("Uživateli $orion_login byl přidělen statut správce");
 			} else {
 				$this->message("Uživateli $orion_login byl odebrán statut správce");
 			}
-		}else {
+		} else {
 			$this->message("Nastala chyba při úpravě statutu uživatele $orion_login", \libs\MessageBuffer::LVL_DNG);
 		}
 		$this->redirectPars('sprava', 'uzivatele');
 	}
-	
-	
-	public function renderInventar(){
+
+	public function renderInventar() {
 		$retired = $this->getParam("retired");
 		$this->addCss("hra.css");
 		$this->template['pageTitle'] = "Správa evidovaných herních krabic";
 		$this->template['gpr'] = 2;
 		$this->template['ipr'] = 2;
-		
-		
+
+
 		$games = Views\GameBoxExtended::fetchAll($this->pdo);
 		$gamesSrt = [];
-		foreach($games as $g){
-			if(!isset($gamesSrt[$g->game_type_id])){
+		foreach ($games as $g) {
+			if (!isset($gamesSrt[$g->game_type_id])) {
 				$gamesSrt[$g->game_type_id] = $g->asArray();
 				$gamesSrt[$g->game_type_id]['tracking_codes'] = [];
 			}
-			if($g->tracking_code && (!$g->retired || $retired)){
+			if ($g->tracking_code && (!$g->retired || $retired)) {
 				$gamesSrt[$g->game_type_id]['tracking_codes'][] = $g;
 			}
 		}
 		$this->template['games'] = $gamesSrt;
 	}
-	
-	public function renderOvladaciPanel(){
+
+	public function renderOvladaciPanel() {
 		$this->template['xml_inventory'] = ['controller' => 'xml', 'action' => 'inventory'];
 		$this->template['xml_reservations'] = ['controller' => 'xml', 'action' => 'reservations'];
 		$this->template['operator_enabled'] = \Dispatcher::ENABLE_OPERATOR;
@@ -161,35 +162,36 @@ class SpravaController extends Controller{
 		$this->template['SQL_action'] = ['controller' => 'operator', 'action' => 'SQL'];
 		$this->template['letiste_links'] = $this->makeLetisteLinks();
 	}
-	
-	private function makeLetisteLinks(){
+
+	private function makeLetisteLinks() {
 		$return = [];
-			$return[] = ['label' => 'Rezervace' ,'url' => ['controller'=>'letiste', 'action'=>'rezervace']];
+		$return[] = ['label' => 'Rezervace', 'url' => ['controller' => 'letiste', 'action' => 'rezervace']];
 		return $return;
 	}
-	
-	public function renderHromadnyMail(){
+
+	public function renderHromadnyMail() {
 		$this->addCss('input-specific.css');
-		$this->addJs ('input-specific.js');
-		
+		$this->addJs('input-specific.js');
+
 		$this->template['default_subject'] = MailManager::getDefaultSubject();
 		$this->template['send_url'] = ['controller' => 'sprava', 'action' => 'poslatMail'];
 		$this->template['games'] = Views\GameTypeExtended::fetchAll($this->pdo);
 	}
-	
-	public function doPoslatMail(){
+
+	public function doPoslatMail() {
 		$gid = $this->getParam('game_type_id', INPUT_POST);
 		$content = $this->getParam('content', INPUT_POST);
 		$users = $this->pdo->subscribedUsersByGame($gid);
 		var_dump($gid, $content, $users);
-		
+
 		$result = MailManager::send($users, $content);
-		if($result['result']){
+		if ($result['result']) {
 			$this->message($result['message'], \libs\MessageBuffer::LVL_SUC);
 		} else {
 			$this->message($result['message'], \libs\MessageBuffer::LVL_DNG);
 		}
-		
+
 		$this->redirectPars('sprava', $this->getDefaultAction());
 	}
+
 }
