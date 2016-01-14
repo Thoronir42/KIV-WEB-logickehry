6d8da36ff25cc17f3167e3bcfdd6b47298143368
Thoronir42 <Thoronir42@Gmail.com>
2016-01-14 11:54:14 +0100

    It's now possible to open/close feedbacks

--- controllers/OhlasController.php
+++ controllers/OhlasController.php
@@ -11,46 +11,69 @@ use \model\database\tables as Tables,
  * @author Stepan
  */
 class OhlasController extends Controller {
-	
+
 	const FEEDBACK_ENABLED = true;
-	
+	const STATE_CLOSE = 'zavrit';
+	const STATE_OPEN = 'otevrit';
+
 	public function __construct($support) {
 		parent::__construct($support);
 	}
-	
+
 	public function startUp() {
 		parent::startUp();
-		if(!$this->user->isLoggedIn()){
+		if (!$this->user->isLoggedIn()) {
 			$this->message('Pro psaní ohlasů musíte být přihlášeni.');
 			$this->redirectPars();
 		}
-		if(!self::FEEDBACK_ENABLED){
+		if (!self::FEEDBACK_ENABLED) {
 			$this->message('Ohlasy zpětné vazby nejsou v tento moment povoleny');
 			$this->redirectPars();
 		}
 		$this->template['pageTitle'] = 'Zpětná vazba';
 	}
 
-	public function renderPridat(){
+	public function renderPridat() {
 		$this->template['feedbackItems'] = Tables\Feedback::fetchAll($this->pdo);
 		$this->template['formAction'] = ['controller' => 'ohlas', 'action' => 'vlozit'];
 		$this->template['detailedList'] = ['controller' => 'ohlas', 'action' => 'zobrazitVsechny'];
 	}
-	
-	public function renderZobrazitVsechny(){
-		$this->template['feedbackItems'] = Tables\Feedback::fetchAll($this->pdo);
-	}
-	
-	public function doVlozit(){
+
+	public function doVlozit() {
 		$fb = Tables\Feedback::fromPOST();
 		$fb->user_id = $this->user->user_id;
 		$fb->created = date(\model\DatetimeManager::DB_FULL);
-		if(Tables\Feedback::insert($this->pdo, $fb->asArray())){
+		if (Tables\Feedback::insert($this->pdo, $fb->asArray())) {
 			$this->message('Vaše zpětná vazba byla úspěšně uložena. Děkujeme.');
 		} else {
 			$this->message('Při ukládání vaší zpětné vazby nastaly potíže.', \libs\MessageBuffer::LVL_WAR);
 		}
 		$this->redirectPars('ohlas', 'pridat');
+	}
+
+	public function renderZobrazitVsechny() {
+		if (!$this->user->isAdministrator()) {
+			$this->message('Detailní výpis ohlasů je dostupný pouze administrátorům', \libs\MessageBuffer::LVL_WAR);
+			$this->redirectPars();
+		}
+		$this->template['feedbackItems'] = Tables\Feedback::fetchAll($this->pdo);
+		$this->template['closeLink'] = ['controller' => 'ohlas', 'action' => 'zmenitStav', 'co' => self::STATE_CLOSE, 'id' => 0];
+		$this->template['openLink'] = ['controller' => 'ohlas', 'action' => 'zmenitStav', 'co' => self::STATE_OPEN, 'id' => 0];
+	}
+
+	public function doZmenitStav() {
+		$co = $this->getParam('co');
+		$id = $this->getParam('id');
+		switch ($co) {
+			default: return;
+			case self::STATE_CLOSE:
+				$result = Tables\Feedback::close($this->pdo, $id);
+				break;
+			case self::STATE_OPEN:
+				$result = Tables\Feedback::open($this->pdo, $id);
+				break;
+		}
+		$this->redirectPars('ohlas', 'zobrazitVsechny');
 	}
 
 }
--- model/database/tables/Feedback.php
+++ model/database/tables/Feedback.php
@@ -34,15 +34,44 @@ class Feedback extends \model\database\DB_Entity {
 	 */
 	public static function insert($pdo, $pars) {
 		$statement = $pdo->prepare('INSERT INTO `web_logickehry_db`.`feedback` '
-		. '(`feedback_type`, `user_id`, `label`, `description`, `created`) '
-		. 'VALUES (:feedback_type, :user_id, :label, :description, :created);');
-		if($statement->execute($pars)){
+				. '(`feedback_type`, `user_id`, `label`, `description`, `created`) '
+				. 'VALUES (:feedback_type, :user_id, :label, :description, :created);');
+		if ($statement->execute($pars)) {
 			return true;
 		}
 		var_dump($pars);
 		echo '<br>';
 		var_dump($statement->errorInfo());
 		return false;
+	}
+
+	/**
+	 * 
+	 * @param \PDO $pdo
+	 * @param int $feedback_id
+	 */
+	public static function close($pdo, $feedback_id) {
+		$statement = $pdo->prepare('UPDATE `web_logickehry_db`.`feedback` '
+				. 'SET resolved = NOW() '
+				. 'WHERE feedback_id = :fid');
+		if ($statement->execute(['fid' => $feedback_id])) {
+			return true;
+		}
+	}
+
+	/**
+	 * 
+	 * @param \PDO $pdo
+	 * @param int $feedback_id
+	 */
+	public static function open($pdo, $feedback_id) {
+		$statement = $pdo->prepare('UPDATE `web_logickehry_db`.`feedback` '
+				. 'SET resolved = NULL '
+				. 'WHERE feedback_id = :fid');
+		if ($statement->execute(['fid' => $feedback_id])) {
+			return true;
+		}
+		var_dump($statement->errorInfo());
 	}
 
 	/**
@@ -79,27 +108,28 @@ class Feedback extends \model\database\DB_Entity {
 		}
 		return $statement;
 	}
-	
-	public function isBugReport(){
+
+	public function isBugReport() {
 		return $this->feedback_type == self::TYPE_BUG;
 	}
-	public function isFeatureSuggestion(){
+
+	public function isFeatureSuggestion() {
 		return $this->feedback_type == self::TYPE_SUGGESTION;
 	}
 
-	public function isResolved(){
+	public function isResolved() {
 		return !is_null($this->resolved);
 	}
-	
-	public function getDate($type){
-		switch($type){
+
+	public function getDate($type) {
+		switch ($type) {
 			default: return 'date error';
-				case 'created':
-				case 'resolved':
-					return date(\model\DatetimeManager::HUMAN_FULL, strtotime($this->$type));
+			case 'created':
+			case 'resolved':
+				return date(\model\DatetimeManager::HUMAN_FULL, strtotime($this->$type));
 		}
 	}
-	
+
 	var $feedback_id;
 	var $feedback_type;
 	var $user_id;
