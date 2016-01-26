eb2fbf5b3848b0a4420741edd5011512926119a8
Thoronir42 <Thoronir42@Gmail.com>
2016-01-26 14:05:58 +0100

    Reservation / Event panel bodies are now switchable

--- www/js/rezervace_vypis.js
+++ www/js/rezervace_vypis.js
@@ -1,39 +1,39 @@
 function setReservationTimes(checkStartTime) {
 	var length = $(".game-type-list-group .active").attr('data-avg-time'),
-		time_from = ($('input[name=time_from]').val()+"").split(':')[0]*1,
-		dayStartHour = $(".time-buttons").attr('data-start-hour'),
-		dayEndHour = $(".time-buttons").attr('data-end-hour');
+			time_from = ($('input[name=time_from]').val() + "").split(':')[0] * 1,
+			dayStartHour = $(".time-buttons").attr('data-start-hour'),
+			dayEndHour = $(".time-buttons").attr('data-end-hour');
 
-	if(checkStartTime && time_from < dayStartHour){
+	if (checkStartTime && time_from < dayStartHour) {
 		time_from = dayStartHour;
 	}
-	
+
 	var hrs = Math.floor(length / 60),
-		mins = length % 60;
+			mins = length % 60;
 
 	//alert('('+length+')'+hrStart+"+"+hrs+">="+endHour);
-	
-	if(time_from + hrs >= dayEndHour){
-		var time = dayEndHour+":00:00";
-		
+
+	if (time_from + hrs >= dayEndHour) {
+		var time = dayEndHour + ":00:00";
+
 	} else {
 		var time = (time_from + hrs);
-		if(time < 10){
+		if (time < 10) {
 			time = '0' + time;
 		}
 		time += ':';
-		if(mins < 10){
+		if (mins < 10) {
 			time += '0';
 		}
-		time += mins+':00';
+		time += mins + ':00';
 	}
-	if(time_from < 10){
-		time_from = '0'+time_from;
+	if (time_from < 10) {
+		time_from = '0' + time_from;
 	}
-	
-	$('input[name=time_from]').val(time_from+":00:00");
+
+	$('input[name=time_from]').val(time_from + ":00:00");
 	$('input[name=time_to]').val(time);
-	
+
 }
 
 $(document).ready(function () {
@@ -49,27 +49,49 @@ $(document).ready(function () {
 		$('input[name=time_from]').val(timeStart);
 		setReservationTimes(false);
 	});
-	
+
 	$(document).on("click", '.dateCol .btn', function () {
 		var year = $(this).attr('data-year'),
-			split = ($(this).find('.date').html()).split('.');
+				split = ($(this).find('.date').html()).split('.');
 		var month = split[1],
-			day = split[0];
-		
-		$('input[name=reservation_date]').val(year+'-'+month+'-'+day);
+				day = split[0];
+
+		$('input[name=reservation_date]').val(year + '-' + month + '-' + day);
 		setReservationTimes(true);
 	});
 
-	$("#filter").change(function(){
+	$("#filter").change(function () {
 		var reservations = $('.reservation');
 		var gid = $(this).val();
-		if(gid === '0'){
+		if (gid === '0') {
 			reservations.removeClass('hidden');
 			return;
 		}
 		reservations.addClass('hidden');
-		var fil = reservations.filter('.game'+gid);
+		var fil = reservations.filter('.game' + gid);
 		fil.removeClass('hidden');
+
+	});
+
+	$("#eventBody").hide();
+
+	$("#toggleEvent").click(function () {
+		var newVal, newLbl, bodyHide, bodyShow;
+		if ($(this).attr("data-toggle") === "false") {
+			newVal = "true";
+			newLbl = "Rezervace";
+			bodyHide = "reservation";
+			bodyShow = "event";
+		} else {
+			newVal = "false";
+			newLbl = "Událost";
+			bodyHide = "event";
+			bodyShow = "reservation";
+		}
+		$(this).attr("data-toggle", newVal);
+		$(this).find(".label").html(newLbl);
+		$("#"+bodyHide+"Body").slideUp();
+		$("#"+bodyShow+"Body").slideDown();
 		
 	});
 
